f54fb58319aa798c61e135131c9ff27fc1254d0c
YohansHailu <95067604+YohansHailu@users.noreply.github.com>
2021-11-26 09:19:31 +0300

    Update palandrom_problem_using_stack.cpp

--- palandrom_problem_using_stack.cpp
+++ palandrom_problem_using_stack.cpp
@@ -1,81 +1,34 @@
-#include <vector>
-
-void print_array(const vector<int> &array)
+#include "stack_problems.cpp"
+#include "stack"
+class pSolution
 {
-    for(int i=0;i<array.size();i++)
-        cout<<array[i]<<" ";
-    cout<<endl;
-}
+    public:
+        bool is_pal(string x,string y); 
+        stack<char> string_to_stack(string s);
+}; 
 
-//driver 
-void merge(const vector<int> &a,vector<int> &sorted,int left,int center,int right)
+bool pSolution::is_pal(string x,string y)
 {
+    if( x.size() != y.size())
+        return false;
+    stack<char> stack_x = string_to_stack(x);
+    for(int i = 0; !stack_x.empty(); i++)
+    {
+        if(stack_x.top() != y[i])
+            return false;
+        stack_x.pop();
+    }
+
+    return true;
     
-
-
 }
-vector<int> merge_sort(const vector<int> &a)
+stack<char> pSolution::string_to_stack(string s)
 {
-   vector<int> sorted(a.size());
-   merge_sort(a,sorted,0,a.size()-1);
-   return sorted;
-}
-
-   
-vector<int> insertion_sort(vector<int> array)
-{
-    for(int p = 1; p<array.size(); p++)
+    stack<char> result;
+    for(int i = 0;i<s.size();i++)
     {
-        int temp = array[p];
-        int i;
-        for(i=p; temp < array[i-1] && i>0 ; i--) 
-            array[i] = array [i-1];
-        array[i] = temp; 
-
-    }
-    return array;
-}
-
-
-void insertion_gaps(vector<int> &array,int gaps)
-{
-    for(int p = gaps; p< array.size(); p++)
-    {
-
-        int i;
-        int temp = array[p];
-        for(i = p; i>=gaps &&temp<array[i-gaps];i-=gaps)
-            array[i] = array[i-gaps];
-        array[i] = temp;
+        result.push(s[i]);
     }
 
-}
-
-vector<int> shellsort(vector<int> array)
-{
-    for(int gap = array.size()/2; gap>0;gap--) 
-    {
-        gap = gap == 2? 1: static_cast<int>(gap/2.2);
-        insertion_gaps(array,gap);
-    }
-    return array;
-}
-// use less
-// I don't think this code works
-vector<int> linear_merge(const vector<int> &a,const vector<int> &b)
-{
-    vector<int> c; 
-    int i = 0;int j = 0;
-    for(;i<a.size() && j<b.size();)
-    {
-        if(a[i] > b[j])
-            c.push_back(b[j++]);
-        else
-            c.push_back(a[i++]);
-    }
-    for(; i<a.size();i++)
-        c.push_back(a[i]);
-    for(; j<b.size();j++)
-        c.push_back(b[j]);
-    return c;
+    return result;
 }
